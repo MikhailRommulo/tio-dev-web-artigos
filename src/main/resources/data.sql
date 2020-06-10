8bb6fba396a611e63ec11d0482e54959a926e849
Mikhail Rommulo <mikahilrommulo@gmail.com>
2020-06-09 23:51:16 -0300

    artigos pré cadastrados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into artigo(titulo,area,hiper_link, data_publicacao) values
+('bitcoin o que é','economia','https://www.bbc.com/portuguese/brasil-42313567','2017-12-11'),('pci express 4.0','hardware','https://www.techtudo.com.br/noticias/2019/06/pci-express-40-conheca-o-padrao-mais-rapido-para-placas-de-video-e-ssds.ghtml','2019-06-06'),
+('API REST','programação','https://becode.com.br/o-que-e-api-rest-e-restful/','2017-02-21'),
+('GraphQL','programação','https://blog.codecasts.com.br/entendendo-o-que-%C3%A9-graphql-atrav%C3%A9s-de-exemplos-c2fa35e8bf63','2019-03-10'),
+('Angular','programação','https://blog.betrybe.com/framework-de-programacao/angular/','2020-04-18');
